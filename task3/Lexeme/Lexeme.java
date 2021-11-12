b5eb433a11230e591b4791bfeb8466ea88db3b15
Yulia778 <84283264+Yulia778@users.noreply.github.com>
2021-11-12 07:43:16 +0300

    Add files via upload

---
+++ task3/Lexeme/Lexeme.java
@@ -0,0 +1,25 @@
+package by.minsk.epam.kozlova.task3.Lexeme;
+
+public class Lexeme {
+    LexemeType type;
+    String value;
+
+    public Lexeme(LexemeType type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    public Lexeme(LexemeType type, Character value) {
+        this.type = type;
+        this.value = value.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "Lexeme{" +
+                "type=" + type +
+                ", value='" + value + '\'' +
+                '}';
+    }
+}
+
---
+++ task3/Lexeme/LexemeType.java
@@ -0,0 +1,8 @@
+package by.minsk.epam.kozlova.task3.Lexeme;
+
+public enum LexemeType {
+    LEFT_BRACKET, RIGHT_BRACKET,
+    OP_PLUS, OP_MINUS, OP_MUL, OP_DIV,
+    NUMBER,
+    EOF;
+}
